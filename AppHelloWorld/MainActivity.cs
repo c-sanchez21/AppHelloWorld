7af7479790069bb542f01dfeb9c43280dffbb0b1
Cesar Sanchez <c_sanchez21@hotmail.com>
2017-03-15 21:28:15 -0700

    Add project files.

---
+++ AppHelloWorld/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AppHelloWorld
+{
+    [Activity(Label = "AppHelloWorld", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            Button b = FindViewById<Button>(Resource.Id.btnMain);
+            b.Click += B_Click;
+        }
+
+        static int count = 0;
+        private void B_Click(object sender, System.EventArgs e)
+        {
+            TextView t = FindViewById<TextView>(Resource.Id.txtViewMain);
+            count++;
+            t.Text = string.Format("Hello World {0}", count);
+        }
+    }
+}
+
